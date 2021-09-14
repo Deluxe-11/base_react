6a3a0150fd0f19fcc4bf96b86544fdd3a1d1ba1b
tuannguyensn2001 <devpro2001@gmail.com>
2021-09-14 07:22:35 +0700

    init

---
+++ pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+
+function Login() {
+  return (
+    <div>
+      <button className={"bg-red-500 text-white p-10"}>hello</button>
+    </div>
+  );
+}
+
+export default Login;
